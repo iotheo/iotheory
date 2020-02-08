47ec353661b7e82172c96c6324c9424d9bd9ef23
John Theo <ioan.theodo@gmail.com>
2020-02-08 17:36:47 +0200

    feat: parse md files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,29 @@
 import { NextPage } from 'next';
+// import matter from 'gray-matter';
 
-interface Props {
+interface IProps {
 
 }
 
-const HomePage: NextPage<Props> = () => <div>Hello world</div>;
+const HomePage: NextPage<IProps> = ctx => {
+  console.log(ctx)
+  return (
+    <div>Hello world</div>
+  )
+};
+
+HomePage.getInitialProps = async () => {
+  const postsPaths = await require.context('../posts/', false, /\.md$/).keys();
+
+  const posts = Promise.all(
+    postsPaths.map(post =>
+      import(`../posts/${post.slice(2)}`).then(r => r.default)
+    )
+  )
+
+  return posts
+    .then(r => r)
+    .catch(err => console.error(err, 'Unable to parse files'))
+}
 
 export default HomePage;
